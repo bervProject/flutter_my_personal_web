ff53bc5f0708ccf32c88d13c7bb4badd48f5fb00
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2025-06-28 03:05:11 +0000

    deploy: 839369b7a2ea57df57d09964f589effdca981d07

--- main.dart.js
+++ main.dart.js
@@ -19025,7 +19025,7 @@ return n}}
 A.a1P.prototype={
 $1(a){var s=A.cU().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/8cd19e509d6bece8ccd74aef027c4ca947363095/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/dd93de6fb1776398bf586cbd477deade1391c7e4/":s)+a},
 $S:61}
 A.JR.prototype={
 d5(a,b){var s,r=this.a
